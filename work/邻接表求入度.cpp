31beab1e2430015eb550e3949c01e23bc2a630cf
leomaokai <leomaokai@163.com>
2020-11-11 22:52:01 +0800

    2020.11.11

--- work/邻接表求入度.cpp
+++ work/邻接表求入度.cpp
@@ -1,41 +1,103 @@
 #include <iostream>
 using namespace std;
 /*
-输入顶点的个数,然后按顶点标号(从0开始)创建邻接表,-1表示空,即没有邻接点了
 
-0--first-->1--next-->2--next-->-1
-1--first-->2--next-->3--next-->-1
-2--first-->1--next-->0--next-->-1
-3--first-->2--next-->1--next-->-1
+0--first-->1--next-->2--next-->nullptr
+1--first-->2--next-->3--next-->nullptr
+2--first-->1--next-->0--next-->nullptr
+3--first-->2--next-->1--next-->nullptr
 
 */
-void solution()
+class graphtest
 {
-    int n = 0; //顶点的个数
-    int m = 0; //邻接点的标号
-    cout << "please input n" << endl;
-    cin >> n;
-    int count[n]{};
-    for (int i = 0; i < n; ++i)
+public:
+    graphtest(int n) : _n(n), graphhead(new head[_n]{}) //构造函数
     {
-        cout << i << "--first-->";
-        while (1)
+    }
+    ~graphtest() //析构函数
+    {
+        for (int i = 0; i < _n; ++i)
         {
-            cin >> m;
-            if (m != -1)
+            listnode *temp = graphhead[i].first;
+            while (temp != nullptr) //链表删除方式进行delete
             {
-                cout << m << "--next-->";
-                count[m]++;
+                temp = graphhead[i].first->next;
+                delete graphhead[i].first;
+                graphhead[i].first = temp;
             }
-            else
-                break;
+        }
+        delete[] graphhead; //delete数组
+    }
+    void insertuv(const int &u, const int &v) //边的插入
+    {
+        listnode *temp = new listnode(); //链表头结点插入方式
+        temp->next = graphhead[u].first;
+        temp->to = v;
+        graphhead[u].first = temp;
+    }
+    void printgraph() //打印邻接表
+    {
+        for (int i = 0; i < _n; ++i)
+        {
+            listnode *temp = graphhead[i].first;
+            cout << i << "--first-->";
+            while (temp != nullptr)
+            {
+                cout << temp->to << "--next--> ";
+                temp = temp->next;
+            }
+            cout << "nullptr" << endl;
         }
     }
-    for (int i = 0; i < n; ++i)
-        cout << count[i] << endl;
-}
+    void solution(); //解题函数
+private:
+    struct listnode //邻接点结构体
+    {
+        int to;
+        listnode *next;
+    };
+    struct head //顶点数组结构体
+    {
+        //int data;
+        listnode *first;
+    };
+    int _n;          //顶点数
+    head *graphhead; //顶点数组
+};
 int main()
 {
-    solution();
+    int n = 0;
+    int e = 0;
+    cout << "input n and e" << endl; //n为顶点数,e为边数
+    cin >> n >> e;
+    graphtest test(n);
+    int u = 0;
+    int v = 0;
+    for (int i = 0; i < e; ++i)
+    {
+        cout << "input u v" << endl; //u-->v
+        cin >> u >> v;
+        test.insertuv(u, v);
+    }
+    test.printgraph();
+    test.solution();
+
     return 0;
+}
+void graphtest::solution()
+{
+    int arry[_n]{};              //存放入度的数组
+    for (int i = 0; i < _n; ++i) //遍历邻接表
+    {
+        listnode *temp = graphhead[i].first;
+        while (temp != nullptr)
+        {
+            int t = temp->to;
+            arry[t]++; //t顶点入度加1
+            temp = temp->next;
+        }
+    }
+    for (int i = 0; i < _n; ++i) //打印结果
+        cout << arry[i] << " ";
+    cout << endl;
 }
